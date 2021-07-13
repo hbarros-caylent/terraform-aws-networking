46d485eb6c5c74b6c9e92c17d8ae1e7597fc5d6b
franco martin <franco.martin@caylent.com>
2021-07-13 16:31:37 -0300

    Removed backend configuration

--- terraform.tf
+++ terraform.tf
@@ -1,15 +1,3 @@
 provider "aws" {
   region = "us-west-1"
 }
-
-terraform {
-  backend "s3" {
-    bucket         = "tamr-tfstate-bucket-franco-networking-2021-06-28"
-    dynamodb_table = "tamr-tfstate-locking-franco-networking-2021-06-28"
-
-    encrypt = "true"
-    key     = "tamr-vpc.tfstate"
-    region  = "us-west-1"
-    profile = "default"
-  }
-}
