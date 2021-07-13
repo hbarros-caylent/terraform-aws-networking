3ecb847351e30139a7727e4532ba2810921e2a79
franco martin <franco.martin@caylent.com>
2021-07-13 16:33:28 -0300

    Removed commented lines

--- examples/public-facing/tamr_vm.tf
+++ examples/public-facing/tamr_vm.tf
@@ -1,15 +1,3 @@
-/*module "tamr_ec2_key_pair" {
-  source     = "terraform-aws-modules/key-pair/aws"
-  version    = "1.0.0"
-  key_name   = format("%s-tamr-ec2-test-key", var.name-prefix)
-  public_key = tls_private_key.tamr_ec2_private_key.public_key_openssh
-}
-
-module "aws-vm-sg-ports" {
-  source = "git::https://github.com/Datatamer/terraform-aws-tamr-vm.git//modules/aws-security-groups?ref=2.0.0"
-  #source = "../../modules/aws-security-groups"
-}*/
-
 module "aws-sg" {
   source = "git::git@github.com:Datatamer/terraform-aws-security-groups.git?ref=1.0.0"
   vpc_id = module.tamr_networking.vpc_id
@@ -38,12 +26,7 @@ module "tamr-vm" {
   vpc_id            = module.tamr_networking.vpc_id
   subnet_id         = module.tamr_networking.tamr_ec2_subnet_id
   bootstrap_scripts = [
-    # NOTE: If you would like to use local scripts, you can use terraform's file() function
-    # file("./test-bootstrap-scripts/install-pip.sh"),
-    # file("./test-bootstrap-scripts/check-install.sh"),
     file("./install-nginx.sh")
-    //data.aws_s3_bucket_object.bootstrap_script.body,
-    //data.aws_s3_bucket_object.bootstrap_script_2.body
   ]
   depends_on = [
     module.tamr_networking
