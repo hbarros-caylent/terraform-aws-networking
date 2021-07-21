ded1f8e25bcf9c071b2a1a67d549cea4b4f7824d
franco martin <franco.martin@caylent.com>
2021-07-21 17:58:18 -0300

    Removed default value and added to the public facing example according to https://github.com/Datatamer/terraform-aws-networking/pull/1#discussion_r673453427

--- examples/public-facing/vpc.tf
+++ examples/public-facing/vpc.tf
@@ -1,5 +1,6 @@
 module "tamr_networking" {
   source                = "../../"
+  vpc_cidr_block        = "10.0.0.0/16"
   ingress_cidr_blocks   = var.ingress_cidr_blocks
   availability_zones    = var.availability_zones
   create_public_subnets = true
