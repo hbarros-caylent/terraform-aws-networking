a4826b0b9c323dfba6945c5b46b0458169102717
franco martin <franco.martin@caylent.com>
2021-07-30 15:02:34 -0300

    Added git source version

--- examples/complete/vpc.tf
+++ examples/complete/vpc.tf
@@ -1,5 +1,6 @@
 module "tamr_networking" {
-    source = "git::https://github.com/Datatamer/terraform-aws-networking.git?ref=1.0"
+    #source = "git::https://github.com/Datatamer/terraform-aws-networking.git?ref=1.0"
+    source = "../../"
     ingress_cidr_blocks = ["172.16.0.0/16"]
     vpc_cidr_block = "10.0.0.0/16"
     data_subnet_cidr_blocks = ["10.0.2.0/24", "10.0.3.0/24"]
