d484933473ec616748ba9f753925ac9a3578123e
franco martin <franco.martin@caylent.com>
2021-08-04 16:55:23 -0300

    Migrated data subnets to intra subnets to avoid internet access

--- vpc.tf
+++ vpc.tf
@@ -2,18 +2,18 @@ locals {
   // Does not include compute subnet
   private_subnets_cidrs = flatten([
     [var.application_subnet_cidr_block],
-    [var.data_subnet_cidr_blocks],
     [local.load_balancing_subnets_cidr_blocks]
   ])
+  intra_subnets_cidrs = var.data_subnet_cidr_blocks
   application_subnet                 = module.vpc.private_subnets[0]
   application_subnet_cidr_block      = module.vpc.private_subnets_cidr_blocks[0]
-  data_subnets                       = [module.vpc.private_subnets[1], module.vpc.private_subnets[2]]
-  data_subnets_cidr_blocks           = [module.vpc.private_subnets_cidr_blocks[2], module.vpc.private_subnets_cidr_blocks[1]]
+  data_subnets                       = [module.vpc.intra_subnets[0], module.vpc.intra_subnets[1]]
+  data_subnets_cidr_blocks           = [module.vpc.intra_subnets_cidr_blocks[1], module.vpc.intra_subnets_cidr_blocks[0]]
   compute_subnet                     = aws_subnet.compute_subnet.id
   compute_subnet_cidr_block          = aws_subnet.compute_subnet.cidr_block
   public_subnets                     = module.vpc.public_subnets
   public_subnets_cidr_blocks         = module.vpc.public_subnets_cidr_blocks
-  load_balancing_subnets             = var.create_load_balancing_subnets ? [module.vpc.private_subnets[4], module.vpc.private_subnets[3]] : []
+  load_balancing_subnets             = var.create_load_balancing_subnets ? [module.vpc.private_subnets[2], module.vpc.private_subnets[1]] : []
   load_balancing_subnets_cidr_blocks = var.create_load_balancing_subnets ? var.load_balancing_subnets_cidr_blocks : []
   public_subnets_cidrs               = var.public_subnets_cidr_blocks
   azs                                = var.availability_zones
@@ -26,6 +26,7 @@ module "vpc" {
   cidr                   = var.vpc_cidr_block
   azs                    = local.azs
   private_subnets        = local.private_subnets_cidrs
+  intra_subnets          = local.intra_subnets_cidrs
   public_subnets         = var.create_public_subnets ? local.public_subnets_cidrs : []
   enable_nat_gateway     = var.enable_nat_gateway && var.create_public_subnets
   single_nat_gateway     = var.enable_nat_gateway
