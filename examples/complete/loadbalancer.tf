ae641abaa333b9c793d6092539a972a065742306
franco martin <franco.martin@caylent.com>
2021-10-21 19:02:56 -0400

    Added EMR master instance id to ALB

--- examples/complete/loadbalancer.tf
+++ examples/complete/loadbalancer.tf
@@ -1,6 +1,7 @@
 module "alb" {
   source              = "../../modules/alb/"
   tls_certificate_arn = var.tls_certificate_arn
+  emr_master_instance_id = module.emr-spark.master_instance.id
   ec2_instance_id     = module.tamr-vm.tamr_instance.ec2_instance_id
   vpc_id              = module.tamr_networking.vpc_id
   subnet_ids          = module.tamr_networking.load_balancing_subnet_ids
