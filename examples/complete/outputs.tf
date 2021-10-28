8ce2ab1ea6e2e8e6a40b4ee6a0379a6dca466156
franco martin <franco.martin@caylent.com>
2021-10-28 16:48:07 -0400

    Added outputs according to https://github.com/Datatamer/terraform-aws-networking/pull/6#discussion_r738703546

--- examples/complete/outputs.tf
+++ examples/complete/outputs.tf
@@ -1,12 +1,3 @@
 output "alb_url" {
   value = "https://${module.alb.load_balancer.dns_name}"
 }
-/*
-output "desired" {
-  value = module.alb.target_groups
-}
-
-output "alb"{
-  value = module.alb.tg_attachments
-}
-*/
