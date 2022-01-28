fecc6c0b596cf8e37ea9c87cc26b5c0dca4e5ac9
Hugo Barros <hugo.barros@caylent.com>
2022-01-28 11:09:15 -0300

    fixes outputs in test_examples

--- test_examples/minimal/outputs.tf
+++ test_examples/minimal/outputs.tf
@@ -1,31 +1,31 @@
 output "vpc_id" {
-  value = module.tamr_networking.vpc_id
+  value = module.tamr_networking.tamr_networking.vpc_id
 }
 
 output "vpc_cidr_block" {
-  value = module.tamr_networking.vpc_cidr_block
+  value = module.tamr_networking.tamr_networking.vpc_cidr_block
 }
 
 output "compute_subnet_id" {
-  value = module.tamr_networking.compute_subnet_id
+  value = module.tamr_networking.tamr_networking.compute_subnet_id
 }
 
 output "application_subnet_id" {
-  value = module.tamr_networking.application_subnet_id
+  value = module.tamr_networking.tamr_networking.application_subnet_id
 }
 
 output "application_subnet_cidr_block" {
-  value = module.tamr_networking.application_subnet_cidr_block
+  value = module.tamr_networking.tamr_networking.application_subnet_cidr_block
 }
 
 output "data_subnet_ids" {
-  value = module.tamr_networking.data_subnet_ids
+  value = module.tamr_networking.tamr_networking.data_subnet_ids
 }
 
 output "tamr_ec2_availability_zone" {
-  value = module.tamr_networking.tamr_ec2_availability_zone
+  value = module.tamr_networking.tamr_networking.tamr_ec2_availability_zone
 }
 
 output "public_subnet_ids" {
-  value = module.tamr_networking.public_subnet_ids
+  value = module.tamr_networking.tamr_networking.public_subnet_ids
 }
